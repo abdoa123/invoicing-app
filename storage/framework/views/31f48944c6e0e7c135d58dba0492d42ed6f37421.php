42936a8011839a8c6535ce3d60ceae7f64f51f69
Abdelrhman mohamed <abdo_201333@hotmail.com>
2023-09-17 18:48:15 +0300

    Complete task #123: Implement user authentication
    In this commit, I've finished implementing user authentication functionality, including user registration and login , also finished Invoice functionality.This task is now complete and ready for review.

---
+++ storage/framework/views/31f48944c6e0e7c135d58dba0492d42ed6f37421.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo e($input['title']); ?></title>
+</head>
+<body>
+    <h1><?php echo e($input['title']); ?></h1>
+    <p><?php echo e($input['body']); ?></p>
+</body>
+</html>
+<?php /**PATH D:\work\Invoice-app\invoicing-app\resources\views/mail/test_mail.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/ae39ce6677704d70eae6fc774382b880ea06a2a7.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laravel Mail Queue Tutorial</title>
+</head>
+<body>
+<p>Hello! This is a test email.</p>
+</body>
+</html><?php /**PATH D:\work\Invoice-app\invoicing-app\resources\views/mails/email.blade.php ENDPATH**/ ?>
